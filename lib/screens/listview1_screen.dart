922c6ea2dc09e0695bb99e4cdacef1574bafd488
Ismael <81421517+IsCa3215@users.noreply.github.com>
2024-11-04 12:36:11 +0100

    options

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
-  
+  final options = const["TARGARUEB", "STARK", "LANNISTER", "BARATHEON"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +11,19 @@ class HomeScreen extends StatelessWidget {
         title: Text("Listview1"),
       ),
       body: ListView(
-         children: const [
-          
+         children: [
+          ...options.map((e) => 
+            ListTile(
+              trailing: Icon(Icons.access_time_sharp),
+              title: Text(e),
+            ),
+          )
+          /*
+            ListTitle(
+              Leading: Icon(Icons:access_time_sharp),
+              title: Text('Hola mundo'),
+            ),
+          */
          ],
       ),
     );
